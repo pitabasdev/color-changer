5fa695761b4a3242c68bd1d99f0f9adf40b4f5e9
pitabas1977 <85897297+pitabas1977@users.noreply.github.com>
2023-05-31 12:14:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [uiColor, setColor] = useState('');
+  function handleChange(event) {
+    setColor(event.target.value);
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>COLOR CHANGER USING REACT JS</h2>
+      <h1>{uiColor}</h1>
+      <div className='app_color' style={{ background: `${uiColor}`, height: '400px', width: '400px', border: '1px solid black', position: 'relative', left: '730px', marginTop: '40px' }}>
+
+      </div>
+      <input type='text' onChange={handleChange}></input>
+
     </div>
   );
 }
